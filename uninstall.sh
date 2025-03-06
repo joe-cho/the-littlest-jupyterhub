f7fdacc5f114e55f2cb01fdb200dd200eb377662
Joe Cho <121422292+joe-cho@users.noreply.github.com>
2025-03-06 09:02:22 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
-sudo systemctl stop jupyterhub
-sudo systemctl disable jupyterhub.service
-
 sudo systemctl stop jupyter-joe.cho
 sudo systemctl disable jupyter-joe.cho.service
+
+sudo systemctl stop jupyterhub
+sudo systemctl disable jupyterhub.service
 
 sudo systemctl stop traefik
 sudo systemctl disable traefik.service
